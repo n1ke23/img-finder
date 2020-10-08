589cd0d644c9690554fc25efcb17c39feb1bedff
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-10-08 13:17:03 +0300

    forma

---
+++ src/js/refs.js
@@ -0,0 +1,3 @@
+export default {
+  apiKey: '18623544-b4f5dbb9e909edd8c116f4e97' 
+}
---
+++ src/js/renderApi.js
@@ -0,0 +1,3 @@
+import apiService from './apiService.js'
+import refs from './refs.js'
+import template from './../template/template.hbs'
